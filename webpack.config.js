673ae6935e3bb6a64344c9111d3df596289afaab
lightningrock <29sxiao@gmail.com>
2022-01-06 19:55:56 +0000

    store

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+module.exports = {
+  entry: path.resolve(__dirname, "src/index.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index_bundle.js",
+    library: {
+        name: "Domputer",
+        type: "umd"
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /(node_modules)|(\.test\.js)/,
+        use: "babel-loader",
+      },
+    ],
+  },
+  mode: "production",
+};
